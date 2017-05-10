6bade34f5e7ab799afed5e85bd2d61ad40bd7b4e
LillyTuckett <lillian_moyle@hotmail.com>
2017-05-10 19:11:57 +1000

    20170510_Final project
    20170510_Final project

--- js/main.js
+++ js/main.js
@@ -1,3 +1,66 @@
+$(document).ready(function(){
+
 $('.hamburger').click(function(){
   $('.menu').toggleClass('open');
+})
+
+$(".one").click(function(){
+		$("#show-this-on-click1").toggleClass("hidden");
+	});	
+
+$(".two").click(function(){
+		$("#show-this-on-click2").toggleClass("hidden");
+	});	
+
+$(".three").click(function(){
+		$("#show-this-on-click3").toggleClass("hidden");
+	});	
+
+$(".four").click(function(){
+		$("#show-this-on-click4").toggleClass("hidden");
+	});	
+$(".five").click(function(){
+		$("#show-this-on-click5").toggleClass("hidden");
+	});	
+$(".six").click(function(){
+		$("#show-this-on-click6").toggleClass("hidden");
+	});	
+$(".seven").click(function(){
+		$("#show-this-on-click7").toggleClass("hidden");
+	});	
+$(".eight").click(function(){
+		$("#show-this-on-click8").toggleClass("hidden");
+	});	
+$(".nine").click(function(){
+		$("#show-this-on-click9").toggleClass("hidden");
+	});	
+$(".ten").click(function(){
+		$("#show-this-on-click10").toggleClass("hidden");
+	});	
+$(".eleven").click(function(){
+		$("#show-this-on-click11").toggleClass("hidden");
+	});	
+$(".twelve").click(function(){
+		$("#show-this-on-click12").toggleClass("hidden");
+	});	
+$(".thirteen").click(function(){
+		$("#show-this-on-click13").toggleClass("hidden");
+	});	
+$(".fourteen").click(function(){
+		$("#show-this-on-click14").toggleClass("hidden");
+	});	
+$(".fifteen").click(function(){
+		$("#show-this-on-click15").toggleClass("hidden");
+	});	
+$(".sixteen").click(function(){
+		$("#show-this-on-click16").toggleClass("hidden");
+	});	
+$(".seventeen").click(function(){
+		$("#show-this-on-click17").toggleClass("hidden");
+	});	
+
+$(".main_image").hover(function(){
+		$("#show-this-on-hover").toggleClass("hidden")
+});
+
 })
